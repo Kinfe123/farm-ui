3e1907a95576bb2e17f28a867b2091b01b711b34
Kinfe123 <kinfishtech@gmail.com>
2024-05-16 12:57:07 +0300

    fix: here revamp

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -3,6 +3,7 @@ import TemplateCard from "components/ui/TemplateCard";
 import { fetchDocs } from "../../firebase/config";
 import { Templates } from "types/types";
 import { allTemplates } from "contentlayer/generated";
+import HeroAnimated from "components/HeroAnimated";
 
 type MetaInfo = {
   title: string;
@@ -28,13 +29,9 @@ export const metadata = {
 };
 
 const Template = async () => {
-  const templates = allTemplates 
- 
+  const templates = allTemplates;
+
   return (
-
-
-
-    
     <>
       <section className="custom-screen mt-20">
         <div className="absolute transform rotate-180 bg-transparent -top-20 z-[-2] h-screen w-screen overflow-hidden  [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#60e_100%)] opacity-20"></div>
@@ -43,26 +40,33 @@ const Template = async () => {
         <HeroBgGradient className="absolute inset-x-0 mx-auto duration-500 top-0 right-0" />
 
         <div className="relative z-10">
-          <div className="max-w-7xl text-white mx-auto space-y-4 text-center">
-            <h1 className="text-center w-full text-5xl md:text-7xl tracking-tighter lg:text-8xl font-display font-bold text-transparent bg-clip-text bg-[linear-gradient(180deg,_#FFF_0%,_rgba(255,_255,_255,_0.00)_202.08%)]  ">
+          <div className="max-w-8xl text-white mx-auto space-y-4 text-center">
+            <HeroAnimated
+              header="Collections of beautifully crafted modern website templates "
+              headerClassName="text-center w-full  text-5xl md:text-7xl tracking-tighter mx-auto lg:text-8xl  font-geist font-normal text-transparent bg-clip-text bg-[linear-gradient(180deg,_#FFF_0%,_rgba(255,_255,_255,_0.00)_202.08%)] leading-4 mt-1"
+              description=" A collection of styled and beautifully designed website
+                templates, built with React, Next.js with Tailwind CSS."
+              descriptionClassName="text-zinc-400 max-w-xl text-sm sm:text-md md:text-lg lg:text-lg mx-auto"
+            >
+              <div className="flex flex-wrap items-center justify-center  gap-3"></div>
+            </HeroAnimated>
+
+            {/* <h1 className="text-center w-full text-5xl md:text-7xl tracking-tighter lg:text-8xl font-display font-bold text-transparent bg-clip-text bg-[linear-gradient(180deg,_#FFF_0%,_rgba(255,_255,_255,_0.00)_202.08%)]  ">
                Collections of beautifully crafted modern website templates 
             
             </h1>
             <p className="text-zinc-400 max-w-xl mx-auto">
               A collection of styled and beautifully designed website
               templates, built with React, Next.js with Tailwind CSS.
-            </p>
+            </p> */}
           </div>
           <ul
             id="templates"
             className="mt-32 text-white space-y-7 divide-y divide-zinc-800 gap-14 grid-cols-1 lg:grid lg:space-y-0 lg:divide-y-0"
           >
-          
-           {templates.map((template) => {
-            return (
-              <TemplateCard  item={template}/>
-            )
-           })}         
+            {templates.map((template) => {
+              return <TemplateCard item={template} />;
+            })}
           </ul>
         </div>
       </section>
