d3aed05dad70d516c8de824354fa653b2300f934
Kinfe123 <kinfishtech@gmail.com>
2024-05-17 00:15:41 +0300

    fix: beta header in nav

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -54,9 +54,9 @@ export default () => {
         >
           <div className="mt-5 max-w-3xl mx-auto border-2 py-5  px-10 backdrop-blur-md  border-white/10 rounded-3xl gp-x-14 items-center md:flex">
             <div className="flex items-center justify-between py-1 md:block">
-            <Link href="/" className="bg-gradient-to-tr from-white/60 via-white/90 to-white/50 text-transparent bg-clip-text font-display font-semibold  text-2xl">
+            <Link href="/" className="relative bg-gradient-to-tr from-white/60 via-white/90 to-white/50 text-transparent bg-clip-text font-display font-semibold  text-2xl">
                  FarmUI 
-              
+                <span className="font-geist text-sm absolute top-0 right-[-40px] z-10 text-white/70">BETA</span>
               </Link>
               <div className="flex md:hidden">
                 <button
