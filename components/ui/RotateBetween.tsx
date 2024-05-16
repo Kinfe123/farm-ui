80ac3eaf04372f294885a12c2eb5b4ef4569d7f4
Kinfe123 <kinfishtech@gmail.com>
2024-05-16 10:31:33 +0300

    feat: rotating text animation

---
+++ components/ui/RotateBetween.tsx
@@ -0,0 +1,35 @@
+import { useEffect , useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+type RotatedString = {
+    rotatedString: string[]
+    className?:string
+}
+
+export default function RotateText({rotatedString , className}: RotatedString) {
+  const words = rotatedString;
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 3000);
+
+    // Clean up interval on unmount
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <AnimatePresence mode="wait">
+      <motion.h1
+        key={words[index]}
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 50 }}
+        transition={{ duration: 0.5 }}
+        className={cn(className , "inline text-center font-display text-5xl md:text-6xl lg:text-8x font-bold tracking-[-0.02em] drop-shadow-sm md:leading-[5rem]")}
+      >
+        {words[index]}
+      </motion.h1>
+    </AnimatePresence>
+  );
+}
--- components/ui/ShowRetro/index.tsx
+++ components/ui/ShowRetro/index.tsx
@@ -1,10 +1,17 @@
 "use client";
 
 import RetroGrid from "../Grid";
+import RotateText from "../RotateBetween";
 const GridSlide = () => {
   return (
-    <div className="relative mb-10 flex md:h-full lg:h-[500px]  w-screen  items-center justify-center overflow-hidden bg-transparent p-20 md:shadow-xl">
-      <span className="font-bold font-display uppercase text-5xl md:text-6xl lg:text-7xl text-center">elevate your web <span className=" bg-gradient-to-tr from-purple-600 via-purple-500/90 to-pink-600 border-input rounded-xl bg-clip-text text-transparent ">experience</span>.</span>
+    <div className="relative font-geist mb-10 flex md:h-full lg:h-[500px]  w-screen  items-center justify-center overflow-hidden bg-transparent p-20 md:shadow-xl">
+      <span className="font-bold font-display uppercase text-5xl md:text-6xl lg:text-8xl text-center">
+        elevate  your web{" "}
+        <span className=" ">
+          <RotateText rotatedString={["BUILD", "SHIPS", "SCALE"]} className="bg-gradient-to-tr from-purple-600 via-purple-500/90 to-pink-600 border-input rounded-xl bg-clip-text text-transparent" />
+        </span>
+        .
+      </span>
       <RetroGrid />
     </div>
   );
