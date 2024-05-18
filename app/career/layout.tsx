110ec84341833eaaf02801a00af3e08b1e356b21
Kinfe123 <kinfishtech@gmail.com>
2024-05-18 13:06:03 +0300

    fix: career layout

---
+++ app/career/layout.tsx
@@ -0,0 +1,14 @@
+export default function CareerLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <div className="overflow-x-hidden">
+        <div className="absolute -z-1 inset-0  h-[300px] w-full bg-transparent opacity-10 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
+  
+        <div className="">{children}</div>
+      </div>
+    );
+  }
+  
