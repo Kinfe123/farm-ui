68982f6eddb35c3dadae99f99260c67954420c17
Kinfe123 <kinfishtech@gmail.com>
2024-05-18 11:21:12 +0300

    fix: template ui revap

---
+++ app/templates/layout.tsx
@@ -0,0 +1,13 @@
+export default function TemplateLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="overflow-x-hidden">
+      <div className="absolute -z-1 inset-0  h-[200px] w-full bg-transparent opacity-5 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
+
+      <div className="">{children}</div>
+    </div>
+  );
+}
