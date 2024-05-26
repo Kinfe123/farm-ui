bf09f74826c15032bc6a2c4915e99635e121aa25
Kinfe123 <kinfishtech@gmail.com>
2024-05-26 10:07:16 +0300

    feat: hero section added

---
+++ previewsComponents/FUIHeroWithGrid.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+
+import LinkItem from "components/ui/LinkItem";
+import { IconGithub } from "components/icons";
+import bghero from "../../../public/img.png";
+import { ChevronRight } from "lucide-react";
+import HeroAnimated from "components/HeroAnimated";
+const FUIHeroWithGrid = () => {
+  return (
+    <>
+      <section className="min-h-[800px] w-full  mt-0 relative">
+        <div className="absolute -z-1 inset-0 opacity-15  h-[600px] w-full bg-transparent  bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+        <img
+          className="absolute inset-x-0 -top-20 opacity-75 "
+          src={
+            "https://pipe.com/_next/image?url=%2Fassets%2Fimg%2Fhero-left.png&w=384&q=75"
+          }
+          width={1000}
+          height={1000}
+          alt="back bg"
+        />
+
+        <div className="relative z-10 max-w-4xl translate-y-[33%]  mx-auto space-y-4">
+          <HeroAnimated
+            header="Take shadcn to the next level for modern web dev experience"
+            headerClassName="text-center max-w-5xl text-2xl md:text-5xl tracking-tighter mx-auto lg:text-7xl font-bold font-geist  font-normal  text-transparent bg-clip-text bg-[linear-gradient(180deg,_#FFF_0%,_rgba(255,_255,_255,_0.00)_202.08%)] leading-0 md:leading-0 md:pb-0 mt-1"
+            description="Move faster with beautiful, responsive UI components and website
+            templates with modern design, 100% free and open-source."
+            descriptionClassName="mx-auto text-zinc-400 text-center text-lg lg:max-w-2xl py-5"
+          >
+            <div className="flex flex-wrap items-center justify-center  gap-3">
+              <LinkItem
+                href="/components"
+                variant="default"
+                className="inline-flex text-center group items-center w-full justify-center bg-gradient-to-bl from-black/90 via-zinc-800 to-black  border-input border-[1px] hover:bg-transparent/10 transition-colors sm:w-auto py-4 px-10"
+              >
+                Browser Components
+                <ChevronRight className="w-4 h-4 ml-2 group-hover:translate-x-1 duration-300" />
+              </LinkItem>
+              <LinkItem
+                href="https://github.com/Kinfe123/farm-ui"
+                variant="shiny"
+                className="inline-flex w-full justify-center items-center gap-x-2 border border-zinc-800 hover:border-zinc-600 bg-zinc-950 hover:text-zinc-100 duration-200 sm:w-auto py-4 px-10"
+                target="_blank"
+              >
+                <IconGithub className="w-5 h-5" />
+                Star on GitHub
+              </LinkItem>
+            </div>
+          </HeroAnimated>
+        </div>
+      </section>
+      {/* <div className="flex justify-center items-center bg-center overflow-x-hidden w-screen absolute top-0 md:-top-2 right-0 min-h-screen">
+        <Image src={bghero} className="w-full bg-center " alt="Hero Image" />
+      </div> */}
+    </>
+  );
+};
+export default FUIHeroWithGrid;
--- viewport/components/reactComponents.tsx
+++ viewport/components/reactComponents.tsx
@@ -1,6 +1,7 @@
 import FUIFaqsWithDividedRows from "previewsComponents/FUIFaqsWithDividedRows";
 import FUIFaqsWithSearchBox from "previewsComponents/FUIFaqsWithSearchBox";
 import FUIForm from "previewsComponents/FUIForm";
+import FUIHeroWithGrid from "previewsComponents/FUIHeroWithGrid";
 import React from "react";
 
 type ComponentProps = {
@@ -26,6 +27,7 @@ export const componentToPreview: ComponentProps = {
 
             
             `
+ 
         }
     },
     "bb87ffac-1579-4670-80be-7c1180404b4e": {
@@ -41,6 +43,20 @@ export const componentToPreview: ComponentProps = {
             `
         }
 
+    },
+
+    "100": {
+        component: <FUIHeroWithGrid />,
+        description: "Hero Section With Grid",
+        path:"previewsComponents/FUIHeroWithGrid",
+        codeCopy: {
+            react: `
+            \nimport React from "react";\nimport * as Avatar from "@radix-ui/react-avatar";\nimport * as ContextMenu from "@radix-ui/react-context-menu";\nimport * as Dialog from "@radix-ui/react-dialog";\nimport * as Select from "@radix-ui/react-select";\nimport * as Tabs from "@radix-ui/react-tabs";\nimport * as DropdownMenu from "@radix-ui/react-dropdown-menu";\nimport { Separator } from "@/components/ui/separator";\nimport { Mail } from "lucide-react";\n\nexport default function FUIFaqsWithSearchBox() {\n  const faqsList = [\n    {\n      q: "What are some random questions to ask?",\n      a: "That's exactly the reason we created this random question generator. There are hundreds of random questions to choose from so you're able to find the perfect random question.",\n      href: "javascript:void(0)",\n    },\n    {\n      q: "Do you include common questions?",\n      a: "This generator doesn't include most common questions. The thought is that you can come up with common questions on your own so most of the questions in this generator.",\n      href: "javascript:void(0)",\n    },\n    {\n      q: "Can I use this for 21 questions?",\n      a: "Yes! there are two ways that you can use this question generator depending on what you're after. You can indicate that you want 21 questions generated.",\n      href: "javascript:void(0)",\n    },\n    {\n      q: "Are these questions for girls or for boys?",\n      a: "The questions in this generator are gender neutral and can be used to ask either male of females (or any other gender the person identifies with).",\n      href: "javascript:void(0)",\n    },\n    {\n      q: "What do you wish you had more talent doing?",\n      a: "If you've been searching for a way to get random questions, you've landed on the correct webpage. We created the Random Question Generator to ask you as many random questions as your heart desires.",\n      href: "javascript:void(0)",\n    },\n  ];\n\n  return (\n    <section className="relative">\n      <img\n        className="absolute inset-x-0 -top-20 opacity-25 "\n        src={\n          "https://pipe.com/_next/image?url=%2Fassets%2Fimg%2Fhero-left.png&w=384&q=75"\n        }\n        width={1000}\n        height={1000}\n        alt="back bg"\n      />\n\n      <div className="absolute -z-1 inset-0  h-[600px] w-full bg-transparent opacity-5 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>\n\n      <div className="pt-10 max-w-screen-xl mx-auto px-4 md:px-8">\n        <div className="space-y-5 sm:text-left sm:max-w-md sm:mr-auto">\n          <h3 className="text-gray-300 font-geist text-3xl font-extrabold sm:text-4xl">\n            How can we help?\n          </h3>\n          <p className="text-gray-100">\n            Everything you need to know about the product. Can"t find the answer\n            you’re looking for? feel free to{" "}\n            <a\n              className="text-cyan-700 font-semibold whitespace-nowrap"\n              href="javascript:void(0)"\n            >\n              contact us\n            </a>\n            .\n          </p>\n          <form\n            onSubmit={(e) => e.preventDefault()}\n            className="mx-auto sm:mx-auto "\n          >\n            <div className="relative">\n              <Mail className="w-6 h-6 text-gray-500 absolute left-3 inset-y-0 my-auto" />\n              <input\n                type="text"\n                placeholder="Enter your email"\n                className="w-full pl-12 pr-3 py-2 text-gray-200 bg-transparent outline-none border focus:border-cyan-600 shadow-sm rounded-lg"\n              />\n            </div>\n          </form>\n        </div>\n        <Separator className="h-[1px] bg-white/10 mt-4" />\n        <div className="mt-12">\n          <ul className="space-y-8 gap-12 grid-cols-2 sm:grid sm:space-y-0 lg:grid-cols-3">\n            {faqsList.map((item, idx) => (\n              <li key={idx} className="space-y-3">\n                <summary className="flex items-center justify-between font-semibold text-gray-500">\n                  {item.q}\n                </summary>\n                <p\n                  dangerouslySetInnerHTML={{ __html: item.a }}\n                  className="text-gray-200 leading-relaxed"\n                ></p>\n                <a\n                  href={item.href}\n                  className="flex items-center gap-x-1 text-sm text-cyan-600 hover:text-cyan-400 duration-150 font-medium"\n                >\n                  Read more\n                  <svg\n                    xmlns="http://www.w3.org/2000/svg"\n                    viewBox="0 0 20 20"\n                    fill="currentColor"\n                    className="w-5 h-5"\n                  >\n                    <path\n                      fillRule="evenodd"\n                      d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"\n                      clipRule="evenodd"\n                    />\n                  </svg>\n                </a>\n              </li>\n            ))}\n          </ul>\n        </div>\n      </div>\n    </section>\n  );\n}\n\n
+
+
+            
+            `
+        }
     },
     "639934f5-ad80-4b22-aa0f-86cc9726a658": {
         component: <FUIFaqsWithDividedRows />,
