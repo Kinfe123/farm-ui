ef7b17f22cdb4fa83423dd90fbb6f1cc368c559a
Kinfe123 <kinfishtech@gmail.com>
2024-06-06 12:38:54 +0300

    vercel log issue

--- apps/www/utils/getBaseURL.ts
+++ apps/www/utils/getBaseURL.ts
@@ -2,6 +2,8 @@ export const getBaseURL = () => {
   const vercelEnv = process.env.NEXT_PUBLIC_VERCEL_ENV;
   const production = process.env.NEXT_PUBLIC_VERCEL_URL;
   const preview = process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL;
+  console.log({vercelEnv , production})
+
   if (vercelEnv === "production") {
     return `https://${production}`;
   } else if (vercelEnv === "preview") {
