95b5b73bd4f76acd78f3f4efa1eb0017c08298fb
Kinfe123 <kinfishtech@gmail.com>
2024-06-09 18:34:25 +0300

    fix: padding leak

--- apps/www/previewsComponents/FUILoginWithGridProvider.tsx
+++ apps/www/previewsComponents/FUILoginWithGridProvider.tsx
@@ -9,7 +9,7 @@ export default function FUILoginWithGridProvider() {
   return (
     <main className="w-full min-h-screen flex flex-col items-center justify-center sm:px-4 relative">
       <div className="absolute top-0 z-[0] h-screen w-screen bg-purple-950/10 bg-[radial-gradient(ellipse_20%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      <div className="w-full  space-y-6 text-gray-600 sm:max-w-md md:max-w-xl lg:max-w-2xl px-5 py-3 rounded-2xl  transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset]">
+      <div className="w-full  space-y-6 text-gray-600 sm:max-w-md md:max-w-xl lg:max-w-2xl px-5 py-10 rounded-2xl  transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset]">
         <div className="text-center">
           <img
             src="https://farmui.com/logo.svg"
