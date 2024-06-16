a143c77660b1fe2d89e4ad68527d4fdea5dd6eb4
Kinfe123 <kinfishtech@gmail.com>
2024-06-16 15:07:21 +0300

    fix: bg overlay

--- apps/www/components/ui/Hero/index.tsx
+++ apps/www/components/ui/Hero/index.tsx
@@ -97,7 +97,10 @@ export default async function () {
           />
 
           {/* Overlay */}
-          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-80"></div>
+          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
+          <div className="absolute top-0 left-0 w-full h-[100px] bg-black opacity-80"></div>
+
+          
         </div>
       </div>
     </>
