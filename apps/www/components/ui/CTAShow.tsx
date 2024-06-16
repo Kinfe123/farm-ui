ae497016b751806a30c434791337dabe4618a220
Kinfe123 <kinfishtech@gmail.com>
2024-06-16 18:44:46 +0300

    fix: ui revamp

--- apps/www/components/ui/CTAShow.tsx
+++ apps/www/components/ui/CTAShow.tsx
@@ -6,56 +6,65 @@ import LinkItem from "./LinkItem";
 import { ChevronRightIcon } from "@heroicons/react/24/solid";
 import BorderGradient from "./Footer/BorderGradient";
 import BgGradient from "./Footer/BgGradient";
-import circle from "/public/circle.png"
+import circle from "/public/circle.png";
 const CTAshow = () => {
   return (
     <div className="mx-h-full relative mx-10 mt-44 z-20">
-      <Image src={circle} alt="circle image" className="hidden sm:block absolute top-0 inset-x-0  -translate-y-32 max-w-full mx-auto"/>
+      <Image
+        src={circle}
+        alt="circle image"
+        className="hidden sm:block absolute top-0 inset-x-0  -translate-y-32 max-w-full mx-auto"
+      />
       <Image
         src={bgback}
         width={900}
         height={900}
         alt="bgback"
-        className="absolute inset-x-0 top-[-300px] z-10 -translate-y-10 max-w-full mx-auto"
+        className="absolute inset-x-0 top-[-300px] z-1 -translate-y-10 max-w-full mx-auto opacity-100"
       />
-       <Image
+      <Image
         src={bgback}
         width={900}
         height={900}
         alt="bgback"
-        className="absolute inset-x-0 top-[-300px] z-10 -translate-x-10 max-w-full mx-auto"
+        className="absolute inset-x-0 top-[-300px] z-1 -translate-x-10 max-w-full mx-auto opacity-100"
       />
-       <Image
+      <Image
         src={bgback}
         width={900}
         height={900}
         alt="bgback"
-        className="absolute inset-x-0 top-[-300px] z-10 translate-x-10 max-w-full mx-auto"
+        className="absolute inset-x-0 top-[-300px] z-1 translate-x-10 max-w-full mx-auto opacity-100"
       />
-       <Image
+      <Image
         src={bgback}
         width={900}
         height={900}
         alt="bgback"
-        className="absolute inset-x-0 top-[-300px] z-1 -translate-x-10 max-w-full mx-auto"
+        className="absolute inset-x-0 top-[-300px] z-1 -translate-x-10 max-w-full mx-auto opacity-100"
       />
       <CTA
         key={2}
-        containerClassName="col-span-1 mt-10 z-5 lg:col-span-3 bg-transparent min-h-[500px] lg:min-h-[600px] xl:min-h-[600px]"
+        containerClassName="relative z-20 bg-page-gradient col-span-1 mt-10 z-5 lg:col-span-3 bg-transparent min-h-[500px] lg:min-h-[600px] xl:min-h-[600px]"
       >
-        <div className="max-w-full mr-auto lg:max-w-3xl lg:mr-auto">
-          <h2 className="max-w-full font-geist font-nomral tracking-tighter md:max-w-5xl lg:max-w-full text-center  lg:text-left text-balance text-2xl sm:text-3xl md:text-5xl lg:text-6xl  bg-gradient-to-tr from-zinc-100 via-zinc-50 to-zinc-100/50 text-transparent bg-clip-text ">
-            Signup for accessing every possible UI that you can imagine at zero
-            cost
+
+        <div className="max-w-full mr-auto lg:max-w-3xl lg:mr-auto z-20">
+        <div className="absolute bittom-0 left-0 w-full h-full bg-black opacity-20 -z-20 "></div>
+          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-10 -z-10"></div>
+
+          <h2 className="max-w-ful mt-8 flex items-center justify-start font-geist font-nomral tracking-tighter md:max-w-5xl lg:max-w-full text-center  lg:text-left text-balance text-2xl sm:text-3xl md:text-5xl lg:text-6xl  bg-gradient-to-tr from-zinc-100 via-zinc-50 to-zinc-100/50 text-transparent bg-clip-text ">
+            Treating UI component as first class citizen cost
           </h2>
-          <p className="mt-4 md:max-w-full  lg:max-w-[26rem] text-center lg:text-left  text-base/6 text-neutral-200">
+          <p className="mt-10 md:max-w-full  lg:max-w-[26rem] text-center lg:text-left text-neutral-200">
             With over 100,000 mothly active bot users, Gippity AI is the most
             popular AI platform for developers.
           </p>
           <LinkItem
             href="/components"
             variant="shiny"
-            className="flex w-fit md:w-fit  mx-auto my-2  px-4 py-5 bg-gradient-to-tr from-zinc-900/90 via-zinc-800/90 to-zinc-700/90 justify-center items-center gap-3  lg:absolute lg:bottom-[-20px]  lg:w-fit hover:bg-zinc-700"
+            // className="flex w-fit md:w-fit  mx-auto my-2  px-4 py-5 bg-hero-gradient justify-center items-center gap-3  lg:absolute lg:bottom-[-20px]  lg:w-fit hover:bg-zinc-700"
+            className="inline-flex rounded-full mt-5 z-20 font-geist text-center group items-center w-full justify-center  bg-gradient-to-tr from-zinc-300/5 via-gray-400/5 to-transparent bg-zinc-900  border-input border-[1px] hover:bg-transparent/10 transition-colors sm:w-auto py-4 px-10"
+          
           >
             Explore components <ChevronRightIcon className="w-5 h-4" />
           </LinkItem>
