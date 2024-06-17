6098096431759cdb67104fb72a039f3a98c340b0
Kinfe123 <kinfishtech@gmail.com>
2024-06-17 10:32:27 +0300

    fix: merge issue

--- apps/www/components/ui/Navbar/Navbar.tsx
+++ apps/www/components/ui/Navbar/Navbar.tsx
@@ -115,6 +115,15 @@ export default () => {
                 })}
               </ul>
               <div className="mt-6 md:mt-0">
+                <LinkItem
+                  variant="shiny"
+                  href="/components"
+                  className="w-full group block bg-gradient-to-tr from-zinc-300/5 via-gray-400/5 to-transparent bg-transparent  border-input border-[1px] hover:bg-transparent/50 "
+                >
+                  Join FarmUI 
+                <ChevronRight className="inline-flex w-4 h-4 ml-2 group-hover:translate-x-1 duration-300" />
+
+                </LinkItem>
                 {!!user ? (
                   <TooltipProvider>
                     <Tooltip>
@@ -144,6 +153,7 @@ export default () => {
                     <ChevronRight className="inline-flex w-4 h-4 ml-2 group-hover:translate-x-1 duration-300" />
                   </LinkItem>
                 )}
+
               </div>
             </div>
           </div>
