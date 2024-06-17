30fc7b8e4b20f3c3697adb37f5ffeca3d508ab43
Kinfe123 <kinfishtech@gmail.com>
2024-06-17 15:53:39 +0300

    fix: height issue

--- apps/www/previewsComponents/FUITestimonialWithThreeColumn.jsx
+++ apps/www/previewsComponents/FUITestimonialWithThreeColumn.jsx
@@ -32,7 +32,7 @@ export default function FUITestimonialWithThreeColumn() {
   ];
 
   return (
-    <section className="relative">
+    <section className="relative min-h-screen">
       <div className="absolute top-0 z-[0] h-screen w-screen bg-purple-950/10 bg-[radial-gradient(ellipse_20%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
       <img
