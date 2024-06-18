9cc26d58144a34d93afc6bcfd11f50f358d31e40
Kinfe123 <kinfishtech@gmail.com>
2024-06-18 18:39:36 +0300

    oath final touch

--- apps/www/auth.ts
+++ apps/www/auth.ts
@@ -2,8 +2,30 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "db";
+import { accounts, sessions, users } from "db/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: DrizzleAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  adapter: DrizzleAdapter(db , {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+
+  }),
   providers: [Google],
+  callbacks: {
+    jwt: ({ token, user }) => {
+      if (user) {
+        const u = user as unknown as any;
+        return {
+          ...token,
+          id: u.id,
+          randomKey: u.randomKey,
+        };
+      }
+      return token;
+    },
+  },
 });
