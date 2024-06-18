012270efbd4d43c5ebc9eada305f923a2e1eb154
Kinfe123 <kinfishtech@gmail.com>
2024-06-18 18:28:16 +0300

    fix: migartion

--- apps/www/db/migrator.ts
+++ apps/www/db/migrator.ts
@@ -11,6 +11,8 @@ const db = drizzle(sql);
 
 async function main(){
     await migrate(db , {migrationsFolder: "./db/migrations" , migrationsTable: "my_migration"})
+    await sql.end()
+    process.exit(0)
 }
 main()
 
