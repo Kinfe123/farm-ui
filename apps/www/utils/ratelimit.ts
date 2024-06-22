3728a14411d43349c2d64eed3cdb4e4750ecd8de
Kinfe123 <kinfishtech@gmail.com>
2024-06-23 00:55:31 +0300

    fix: exposed db from redis

--- apps/www/utils/ratelimit.ts
+++ apps/www/utils/ratelimit.ts
@@ -1,8 +1,13 @@
 import { Ratelimit } from "@upstash/ratelimit"
 import { Redis } from "@upstash/redis"
+export const db: Redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!
+});
 
 export const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
+  redis: db,
   limiter: Ratelimit.slidingWindow(1, "30 m")
 
 })
+
