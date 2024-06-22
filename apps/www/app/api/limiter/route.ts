b0f52170982754b1b922e50b7521967aa8939d76
Kinfe123 <kinfishtech@gmail.com>
2024-06-22 22:28:10 +0300

    fix: fixing weirdo sym

--- apps/www/app/api/limiter/route.ts
+++ apps/www/app/api/limiter/route.ts
@@ -1,18 +1,17 @@
-import { NextRequest, NextResponse } from "next/server"
-import { ratelimit } from "@/lib/rate-limit"
+import { NextRequest, NextResponse } from "next/server";
+import { ratelimit } from "utils/ratelimit";
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
-    const res = await req.json()
-    const ip = req.headers.get('x-forwarded-for') ?? ''
-    const data = await ratelimit.limit(ip)
+    const res = await req.json();
+    const ip = req.headers.get("x-forwarded-for") ?? "";
+    const data = await ratelimit.limit(ip);
     if (!data.success) {
       return new NextResponse("You can only send 1 req / 30min.", {
         status: 429,
-      })
+      });
     }
-    return new Response("The data logged: " + JSON.stringify(res))
-
+    return new Response("The data logged: " + JSON.stringify(res));
   } catch (err) {
-    console.log('Error has occured : ', err)
+    console.log("Error has occured : ", err);
   }
-} '''
+}
