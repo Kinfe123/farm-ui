4d9a67881c22be51aae88db5c8e7d9de153c0387
Kinfe123 <kinfishtech@gmail.com>
2024-06-22 22:19:43 +0300

    fix: add ratelimter

---
+++ apps/www/app/api/limiter/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server"
+import { ratelimit } from "@/lib/rate-limit"
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const res = await req.json()
+    const ip = req.headers.get('x-forwarded-for') ?? ''
+    const data = await ratelimit.limit(ip)
+    if (!data.success) {
+      return new NextResponse("You can only send 1 req / 30min.", {
+        status: 429,
+      })
+    }
+    return new Response("The data logged: " + JSON.stringify(res))
+
+  } catch (err) {
+    console.log('Error has occured : ', err)
+  }
+} '''
