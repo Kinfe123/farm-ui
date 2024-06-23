a944c9e9fe4d92c05ba074a18cd725bb7d45a811
Kinfe123 <kinfishtech@gmail.com>
2024-06-23 22:40:11 +0300

    fix: ui opacity change

--- apps/www/components/GlareCard.tsx
+++ apps/www/components/GlareCard.tsx
@@ -121,7 +121,7 @@ export const GlareCard = ({
         }
       }}
     >
-      <div className="h-full  grid will-change-transform origin-center transition-transform duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] [transform:rotateY(var(--r-x))_rotateX(var(--r-y))] rounded-[var(--radius)] border border-slate-800 hover:[--opacity:0.3] hover:[--duration:200ms] hover:[--easing:linear] hover:filter-none overflow-hidden">
+      <div className="h-full  grid will-change-transform origin-center transition-transform duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] [transform:rotateY(var(--r-x))_rotateX(var(--r-y))] rounded-[var(--radius)] border border-slate-800 hover:[--opacity:0.2] hover:[--duration:200ms] hover:[--easing:linear] hover:filter-none overflow-hidden">
         <div className="w-full h-full grid [grid-area:1/1] mix-blend-soft-light [clip-path:inset(0_0_0_0_round_var(--radius))]">
           <div className={cn("h-full w-full bg-hero-gradient", className)}>
             {children}
