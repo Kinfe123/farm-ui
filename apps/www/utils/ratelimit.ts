c1a2e1b12fa5ecaabb823aba2da2ac17476ffe64
Kinfe123 <kinfishtech@gmail.com>
2024-06-23 23:15:30 +0300

    fix: prod issue

--- apps/www/utils/ratelimit.ts
+++ apps/www/utils/ratelimit.ts
@@ -1,8 +1,8 @@
 import { Ratelimit } from "@upstash/ratelimit"
 import { Redis } from "@upstash/redis"
 export const db: Redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!
+  url: process.env.UPSTASH_REDIS_REST_URL ?? "",
+  token: process.env.UPSTASH_REDIS_REST_TOKEN ?? ""
 });
 
 export const ratelimit = new Ratelimit({
