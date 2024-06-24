51043e13bff4236d964a3aec510f7e3eb25b575b
Kinfe123 <kinfishtech@gmail.com>
2024-06-24 19:05:33 +0300

    fix: tempo disabling

--- apps/www/app/api/limiter/route.ts
+++ apps/www/app/api/limiter/route.ts
@@ -1,17 +1,18 @@
-import { ratelimit } from "utils/ratelimit";
+// import { ratelimit } from "utils/ratelimit";
 export async function POST(req: Request) {
-  try {
-    const ip = req.headers.get("x-forwarded-for") ?? "";
-    const data = await ratelimit.limit(ip);
-    if (!data.success) {
-      return new Response("You can only send 1 req / 30min.", {
-        status: 429,
-      });
-    }
-    return new Response("Made an attempt");
-  } catch (err) {
-    console.log("Error has occured  ", err);
-    return new Response("Error has occured", { status: 404 });
-  }
+  return new Response("To be implemented again, " , {status:429})
+  // try {
+  //   const ip = req.headers.get("x-forwarded-for") ?? "";
+  //   const data = await ratelimit.limit(ip);
+  //   if (!data.success) {
+  //     return new Response("You can only send 1 req / 30min.", {
+  //       status: 429,
+  //     });
+  //   }
+  //   return new Response("Made an attempt");
+  // } catch (err) {
+  //   console.log("Error has occured  ", err);
+  //   return new Response("Error has occured", { status: 404 });
+  // }
 }
 
