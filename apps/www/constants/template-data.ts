fb8d556f6e301bdaa9ff3e437881e578c7dd215c
Kinfe123 <kinfishtech@gmail.com>
2024-10-12 17:29:05 +0300

    chore: link upatee

--- apps/www/constants/template-data.ts
+++ apps/www/constants/template-data.ts
@@ -2,7 +2,7 @@ export const TemplateDataBilling = {
   docy: "https://9697820927999.gumroad.com/l/docy",
   perkai: "https://9697820927999.gumroad.com/l/perkai",
   ease: "https://9697820927999.gumroad.com/l/ease",
-  sharp: "",
+  sharp: "https://9697820927999.gumroad.com/l/sharps",
   spotter: "",
   arc: "https://9697820927999.gumroad.com/l/arc",
   curve: "https://9697820927999.gumroad.com/l/curve",
