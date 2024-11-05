1decb04fc9b97484b2ea9dee5084158abc118e84
Kinfe123 <kinfishtech@gmail.com>
2024-11-05 22:02:34 +0300

    fix: wrapper prose config

--- apps/www/app/(www)/changelog/page.tsx
+++ apps/www/app/(www)/changelog/page.tsx
@@ -47,10 +47,10 @@ const ChangeLogPage = () => {
                 </span>{" "}
                 and{" "}
                 <span className="bg-gradient-to-r from-white to-rangepurple-200 text-transparent bg-clip-text">
-                  All the jazz will
+                  All the jazz
                 </span>{" "}
                 <span className="text-transparent bg-clip-text bg-[linear-gradient(180deg,_#FFF_0%,_rgba(255,_255,_255,_0.00)_202.08%)] ">
-                  <br /> be dumped here.
+                  <br /> dumped here.
                 </span>
               </h2>
               <p className="max-w-2xl mr-auto text-gray-300">
@@ -64,23 +64,10 @@ const ChangeLogPage = () => {
                 Browse More
                 <ChevronRight className="inline-flex justify-center items-center  w-4 h-4 ml-2 group-hover:translate-x-1 duration-300" />
               </LinkItem>
-              {/* <div className="items-start z-30 justify-start gap-x-3 space-y-3 sm:flex sm:space-y-0">
-                <span className="relative inline-block overflow-hidden rounded-full p-[1.5px]">
-                  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                  <div className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-gray-950  text-xs font-medium text-gray-50 backdrop-blur-3xl">
-                    <a
-                      href="/components"
-                      className="inline-flex rounded-full text-left group items-center w-full justify-center   bg-gradient-to-tr from-zinc-300/5 via-purple-400/20 to-transparent    text-white border-input border-[1px] hover:bg-transparent/90 transition-colors sm:w-auto py-4 px-10"
-                    >
-                      Browse components
-                    </a>
-                  </div>
-                </span>
-              </div> */}
             </div>
           </div>
         </section>
-        <div className="h-full ml-[80px]">
+        <div className="h-full  ml-[80px]">
           <ChangelogDisplay />
         </div>{" "}
       </div>
--- apps/www/components/MdxComponent.tsx
+++ apps/www/components/MdxComponent.tsx
@@ -1,115 +1,150 @@
 // @ts-nocheck
-import Image from "next/image"
-import { useMDXComponent } from "next-contentlayer/hooks"
+import Image from "next/image";
+import { useMDXComponent } from "next-contentlayer/hooks";
 import { Callout } from "components/Callout";
-import { MdxTabs , MdxTab } from "components/MdxTab";
+import { MdxTabs, MdxTab } from "components/MdxTab";
 import { cn } from "@/lib/utils";
-import { toast  , useToast} from "@/components/ui/use-toast";
+import { toast, useToast } from "@/components/ui/use-toast";
 export const components = {
- 
   h1: ({ className, ...props }) => (
-      <h1
-          className={cn("mt-2 scroll-m-20 text-4xl font-semibold font-geist tracking-normal", className)}
-          {...props}
-      />
+    <h1
+      className={cn(
+        "mt-2 scroll-m-20 text-4xl font-semibold font-geist tracking-normal",
+        className
+      )}
+      {...props}
+    />
   ),
   h2: ({ className, ...props }) => (
-      <h2
-          className={cn(
-              "mt-6 scroll-m-20 border-b pb-1 text-3xl font-normal font-geist tracking-tight first:mt-0",
-              className,
-          )}
-          {...props}
-      />
+    <h2
+      className={cn(
+        "mt-6 scroll-m-20 border-b pb-1 text-3xl font-normal font-geist tracking-tight first:mt-0",
+        className
+      )}
+      {...props}
+    />
   ),
   h3: ({ className, ...props }) => (
-      <h3
-          className={cn("mt-4 scroll-m-20 text-2xl font-semibold tracking-normal", className)}
-          {...props}
-      />
+    <h3
+      className={cn(
+        "mt-4 scroll-m-20 text-2xl font-semibold tracking-normal",
+        className
+      )}
+      {...props}
+    />
   ),
   h4: ({ className, ...props }) => (
-      <h4
-          className={cn("mt-4 scroll-m-20 text-xl font-semibold tracking-tight", className)}
-          {...props}
-      />
+    <h4
+      className={cn(
+        "mt-4 scroll-m-20 text-xl font-semibold tracking-tight",
+        className
+      )}
+      {...props}
+    />
   ),
   h5: ({ className, ...props }) => (
-      <h5
-          className={cn("mt-4 scroll-m-20 text-lg font-semibold tracking-tight", className)}
-          {...props}
-      />
+    <h5
+      className={cn(
+        "mt-4 scroll-m-20 text-lg font-semibold tracking-tight",
+        className
+      )}
+      {...props}
+    />
   ),
   h6: ({ className, ...props }) => (
-      <h6
-          className={cn("mt-4 scroll-m-20 text-base font-semibold tracking-tight", className)}
-          {...props}
-      />
+    <h6
+      className={cn(
+        "mt-4 scroll-m-20 text-base font-semibold tracking-tight",
+        className
+      )}
+      {...props}
+    />
   ),
   a: ({ className, ...props }) => (
-      <a className={cn("font-medium underline underline-offset-4", className)} {...props} />
+    <a
+      className={cn("font-medium underline underline-offset-4", className)}
+      {...props}
+    />
   ),
   p: ({ className, ...props }) => (
-      <p className={cn("leading-7 [&:not(:first-child)]:mt-6", className)} {...props} />
+    <p
+      className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
+      {...props}
+    />
   ),
   ul: ({ className, ...props }) => (
-      <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
+    <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
   ),
   ol: ({ className, ...props }) => (
-      <ol className={cn("my-6 ml-6 list-decimal", className)} {...props} />
+    <ol className={cn("my-6 ml-6 list-decimal", className)} {...props} />
   ),
-  li: ({ className, ...props }) => <li className={cn("mt-2", className)} {...props} />,
+  li: ({ className, ...props }) => (
+    <li className={cn("mt-2", className)} {...props} />
+  ),
   blockquote: ({ className, ...props }) => (
-      <blockquote
-          className={cn("mt-6 border-l-2 pl-6 italic [&>*]:text-muted-foreground", className)}
-          {...props}
-      />
+    <blockquote
+      className={cn(
+        "mt-6 border-l-2 pl-6 italic [&>*]:text-muted-foreground",
+        className
+      )}
+      {...props}
+    />
   ),
-  img: ({ className, alt, ...props }: React.ImgHTMLAttributes<HTMLImageElement>) => (
-      // rome-ignore lint/a11y/useAltText: <explanation>
-      <img className={cn("rounded-md border", className)} alt={alt} {...props} />
+  img: ({
+    className,
+    alt,
+    ...props
+  }: React.ImgHTMLAttributes<HTMLImageElement>) => (
+    // rome-ignore lint/a11y/useAltText: <explanation>
+    <img className={cn("rounded-md border", className)} alt={alt} {...props} />
   ),
   hr: ({ ...props }) => <hr className="my-4 md:my-8" {...props} />,
   table: ({ className, ...props }: React.HTMLAttributes<HTMLTableElement>) => (
-      <div className="my-6 w-full overflow-y-auto">
-          <table className={cn("w-full", className)} {...props} />
-      </div>
+    <div className="my-6 w-full overflow-y-auto">
+      <table className={cn("w-full", className)} {...props} />
+    </div>
   ),
   tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
-      <tr className={cn("m-0 border-t p-0 even:bg-muted", className)} {...props} />
+    <tr
+      className={cn("m-0 border-t p-0 even:bg-muted", className)}
+      {...props}
+    />
   ),
   th: ({ className, ...props }) => (
-      <th
-          className={cn(
-              "border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right",
-              className,
-          )}
-          {...props}
-      />
+    <th
+      className={cn(
+        "border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right",
+        className
+      )}
+      {...props}
+    />
   ),
   td: ({ className, ...props }) => (
-      <td
-          className={cn(
-              "border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
-              className,
-          )}
-          {...props}
-      />
+    <td
+      className={cn(
+        "border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
+        className
+      )}
+      {...props}
+    />
   ),
   pre: ({ className, ...props }) => (
-      <pre
-          className={cn("mb-4 mt-6 overflow-x-auto rounded-lg border bg-black py-4", className)}
-          {...props}
-      />
+    <pre
+      className={cn(
+        "mb-4 mt-6 overflow-x-auto rounded-lg border bg-black py-4",
+        className
+      )}
+      {...props}
+    />
   ),
   code: ({ className, ...props }) => (
-      <code
-          className={cn(
-              "relative rounded border px-[0.3rem] py-[0.2rem] font-mono text-sm",
-              className,
-          )}
-          {...props}
-      />
+    <code
+      className={cn(
+        "relative rounded border px-[0.3rem] py-[0.2rem] font-mono text-sm",
+        className
+      )}
+      {...props}
+    />
   ),
   // Steps,
   toast,
@@ -121,11 +156,11 @@ export const components = {
 };
 
 interface MdxProps {
-  code: string
+  code: string;
 }
 
 export function Mdx({ code }: MdxProps) {
-  const Component = useMDXComponent(code)
+  const Component = useMDXComponent(code);
 
-  return <Component components={components} />
+  return <Component components={components} />;
 }
